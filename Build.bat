ae830e187f44150c22612f1052c89e21d442615c
Raunak Pradip Shah <raunakshah13@gmail.com>
2016-11-30 01:00:54 +0530

    Batch file for compilation

--- Build.bat
+++ Build.bat
@@ -1,4 +1,5 @@
 :Run bat file to compile lex and yacc
 flex C_lang.l
+:Path of bison parser should be added below
 "C:\users\pcs\downloads\win_flex_bison-latest\win_bison"  C_lang.y
 gcc lex.yy.c C_lang.tab.c graph.c
